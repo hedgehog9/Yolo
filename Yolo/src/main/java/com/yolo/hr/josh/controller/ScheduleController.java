757943747088e54e7117ccc14af6673aa7e3fda1
tkddns6621 <tkddns6007@gmail.com>
2022-11-20 20:24:14 +0900

    캘린더 CRUD 완료 추후 CSS 수정

--- Yolo/src/main/java/com/yolo/hr/josh/controller/ScheduleController.java
+++ Yolo/src/main/java/com/yolo/hr/josh/controller/ScheduleController.java
@@ -1,17 +1,22 @@
 package com.yolo.hr.josh.controller;
 
+
+
 import java.util.*;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.yolo.hr.josh.model.MemberVO;
+import com.yolo.hr.josh.model.ScheduleVO;
 import com.yolo.hr.josh.service.InterScheduleService;
 
 @Controller
@@ -19,6 +24,17 @@ public class ScheduleController {
 
 	@Autowired
 	private InterScheduleService service;
+	
+	@RequestMapping(value="/schedule/calendar.yolo")
+	public String calendar(HttpServletRequest request) {
+		MemberVO loginuser = new MemberVO();
+		loginuser.setEmpno("1");
+		loginuser.setDeptno("1");
+		HttpSession session = request.getSession();
+		session.setAttribute("loginuser", loginuser);
+		
+		return "josh/calendar.admin";
+	}
 	
 	@ResponseBody
 	@RequestMapping(value="/schedule/insertSchedule/searchJoinUserList.yolo", produces="text/plain;charset=UTF-8") 
@@ -46,4 +62,165 @@ public class ScheduleController {
 		
 		return jsonArr.toString();
 	}
+	
+	// 켈린더에 일정을 등록하는 메소드
+	@ResponseBody
+	@RequestMapping(value="/schedule/insertSchedule.yolo", produces="text/plain;charset=UTF-8", method = {RequestMethod.POST})
+	public String insertSchedule(HttpServletRequest request, ScheduleVO scvo) {
+		
+		String category = scvo.getCategory();
+		
+		// System.out.println("확인용 공유자들" + scvo.getJoinuser());
+		
+		switch (category) { // 카테고리에 따라 색상 주기
+		case "회의":
+			scvo.setColor("blue");
+			break;
+
+		case "미팅":
+			scvo.setColor("red");	
+			break;
+		case "출장":
+			scvo.setColor("gray");	
+			break;
+		}
+		
+		int n = service.insertSchedule(scvo);
+		
+		JSONObject jsonObj = new JSONObject();
+		
+		jsonObj.put("n", n);
+		
+		return jsonObj.toString();
+	}
+	
+	
+	// 켈린더에 일정을 수정하는 메소드
+	@ResponseBody
+	@RequestMapping(value="/schedule/updateSchedule.yolo", produces="text/plain;charset=UTF-8", method = {RequestMethod.POST})
+	public String updateSchedule(HttpServletRequest request, ScheduleVO scvo) {
+		
+		String category = scvo.getCategory();
+		
+		// System.out.println("확인용 공유자들" + scvo.getJoinuser());
+		
+		switch (category) { // 카테고리에 따라 색상 주기
+		case "회의":
+			scvo.setColor("blue");
+			break;
+			
+		case "미팅":
+			scvo.setColor("red");	
+			break;
+		case "출장":
+			scvo.setColor("gray");	
+			break;
+		}
+		
+		//String schedule_no = scvo.getSchedule_no();
+		//System.out.println("확인용 schedule_no => " + schedule_no);
+		
+		int n = service.updateSchedule(scvo);
+		
+		JSONObject jsonObj = new JSONObject();
+		
+		jsonObj.put("n", n);
+		
+		return jsonObj.toString();
+	}
+	
+	// 캘린더를 삭제하는 메소드
+	@ResponseBody
+	@RequestMapping(value="/schedule/deleteSchedule.yolo", produces="text/plain;charset=UTF-8", method = {RequestMethod.POST})
+	public String deleteSchedule(HttpServletRequest request) {
+		
+		String schedule_no = request.getParameter("schedule_no");
+		//System.out.println("확인용 schedule_no => " + schedule_no);
+		
+		int n = service.deleteSchedule(schedule_no);
+		
+		JSONObject jsonObj = new JSONObject();
+		
+		jsonObj.put("n", n);
+		
+		return jsonObj.toString();
+	}
+	
+	
+	// 켈린더를 모두 가져오는 메소드
+	@ResponseBody
+	@RequestMapping(value="/schedule/selectScheduleList.yolo", produces="text/plain;charset=UTF-8")
+	public String selectSchedule() {
+		
+		List<ScheduleVO> selectScheduleList = service.selectScheduleList();
+		
+		JSONArray jsonArr = new JSONArray();
+		
+		if(selectScheduleList != null) {
+			
+			for(ScheduleVO scvo : selectScheduleList) {
+				JSONObject jsonObj = new JSONObject();
+				jsonObj.put("schedule_no", scvo.getSchedule_no());
+				jsonObj.put("fk_empno", scvo.getFk_empno());
+				jsonObj.put("start_date", scvo.getStart_date());
+				jsonObj.put("end_date", scvo.getEnd_date());
+				jsonObj.put("subject", scvo.getSubject());
+				jsonObj.put("content", scvo.getContent());
+				jsonObj.put("color", scvo.getColor());
+				jsonObj.put("category", scvo.getCategory());
+				jsonObj.put("fk_deptno", scvo.getFk_deptno());
+				jsonObj.put("joinuser", scvo.getJoinuser());
+				
+				jsonArr.put(jsonObj);
+			}
+			
+		}
+		
+		return jsonArr.toString();
+	}
+	
+	
+	// 특정 하나 켈린더를 가져오는 메소드
+	@ResponseBody
+	@RequestMapping(value="/schedule/selectDetailSchedule.yolo", produces="text/plain;charset=UTF-8")
+	public String selectDetailSchedule(HttpServletRequest request) {
+		
+		String schedule_no = request.getParameter("schedule_no");
+		// System.out.println("확인용 : " + schedule_no);
+		
+		ScheduleVO scvo = service.selectDetailSchedule(schedule_no);
+		
+		JSONObject jsonObj = new JSONObject();
+		jsonObj.put("schedule_no", scvo.getSchedule_no());
+		jsonObj.put("fk_empno", scvo.getFk_empno());
+		jsonObj.put("start_date", scvo.getStart_date());
+		jsonObj.put("end_date", scvo.getEnd_date());
+		jsonObj.put("subject", scvo.getSubject());
+		jsonObj.put("content", scvo.getContent());
+		jsonObj.put("color", scvo.getColor());
+		jsonObj.put("category", scvo.getCategory());
+		jsonObj.put("fk_deptno", scvo.getFk_deptno());
+		jsonObj.put("joinuser", scvo.getJoinuser());
+		jsonObj.put("place", scvo.getPlace());
+		
+		return jsonObj.toString();
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
