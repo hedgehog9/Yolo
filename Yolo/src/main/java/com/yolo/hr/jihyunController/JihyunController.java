fdc4fba24e9378108f05df74ae72a2d6dc4b385b
jyeost <pig9619@naver.com>
2022-11-23 15:41:28 +0900

    chore : 지현 - 민지언니 파일 가져오고, 휴가 패키지 삭제

--- Yolo/src/main/java/com/yolo/hr/jihyunController/JihyunController.java
+++ Yolo/src/main/java/com/yolo/hr/jihyunController/JihyunController.java
@@ -15,4 +15,43 @@ public class JihyunController {
 	public String receivedMessage() {
 		return "jihyun/messenger/receivedMessage.admin";
 	}
+	
+	
+	// 관리자 내휴가 휴가 개요
+		@RequestMapping(value="leaveSummary.yolo") 
+	    public String leaveSummary() {
+			
+	        return "jihyun/leave/leaveSummary.admin"; // 뷰단 페이지
+	        // /WEB-INF/views/admin/{1}/{2}.jsp
+	        
+	    }
+		
+		// 관리자 내휴가 연차 상세 
+		@RequestMapping(value="leaveDetail.yolo") 
+	    public String leaveDetail() {
+			
+	        return "jihyun/leave/leaveDetail.admin";
+	    }
+		
+		
+		// 관리자 구성원 휴가 보유 현황
+		@RequestMapping(value="empLeaveStatus.yolo") 
+	    public String empLeaveStatus() {
+			
+	        return "jihyun/leave/empLeaveStatus.admin"; // 뷰단 페이지
+	    }
+		
+		// 관리자 구성원 휴가 사용 내역
+		@RequestMapping(value="empLeaveUsingList.yolo") 
+	    public String empLeaveUsingList() {
+			
+	        return "jihyun/leave/empLeaveUsingList.admin"; // 뷰단 페이지
+	    }
+		
+		// 관리자 구성원 연차 촉진 	
+		@RequestMapping(value="empLeavePromotion.yolo") 
+	    public String empLeavePromotion() {
+			
+	        return "jihyun/leave/empLeavePromotion.admin";
+	    }
 }
