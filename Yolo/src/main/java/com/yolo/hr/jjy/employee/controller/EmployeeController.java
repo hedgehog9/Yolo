055b8108ecaf628fd616ea18024f6f9184af83ef
hedgehog9 <redpwon@gmail.com>
2022-12-08 11:49:52 +0900

    fix: 진영- 휴직 처리시 새로운 소식에 insert 되도록 수정

--- Yolo/src/main/java/com/yolo/hr/jjy/employee/controller/EmployeeController.java
+++ Yolo/src/main/java/com/yolo/hr/jjy/employee/controller/EmployeeController.java
@@ -3,6 +3,7 @@ package com.yolo.hr.jjy.employee.controller;
 import java.util.*;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.json.*;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.yolo.hr.jjy.employee.model.EmployeeVO;
 import com.yolo.hr.jjy.employee.model.InterEmployeeDAO;
 import com.yolo.hr.jjy.employee.service.InterEmployeeService;
 
@@ -112,7 +114,7 @@ public class EmployeeController {
 		// System.out.println("############## 확인용 ############"+totalPage);
 
 		JSONObject jsonObj = new JSONObject();
-		jsonObj.put("totalPage",totalPage); // 1 이상인경우 휴직 신청 불가 
+		jsonObj.put("totalPage",totalPage); 
 		
 		return jsonObj.toString();
 	}
@@ -250,13 +252,19 @@ public class EmployeeController {
 	// 휴직 처리 
 	@ResponseBody
 	@RequestMapping(value = "/leaveAbsence.yolo", produces = "text/plain;charset=UTF-8")
-	public String leaveabsence(HttpServletRequest request) {
-
+	public String addAlarm_leaveabsence(Map<String, String> paraMap, HttpServletRequest request) {
+		
 		String leavetype = request.getParameter("leavetype");
 		String startdate = request.getParameter("startdate");
 		String enddate = request.getParameter("enddate");
 		String memo = request.getParameter("memo");
 		String empno = request.getParameter("empno");
+		
+		paraMap.put("fk_recipientno ", empno ); // 받는사람 (여러명일때는 ,으로 구분된 str)
+		paraMap.put("url", "userDetail.yolo?empno=" );
+		paraMap.put("url2", empno ); // 연결되는 pknum등...  (여러개일때는 ,으로 구분된 str)(대신 받는 사람 수랑 같아야됨)
+		paraMap.put("alarm_content", "휴직 처리되었습니다." );
+		paraMap.put("alarm_type", "4" );
 		
 		Map<String,String> leaveMap = new HashMap<>();
 		
